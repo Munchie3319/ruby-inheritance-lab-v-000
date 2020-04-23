a8bd52e9efa65467e3399d3e2dc7a86a402a44ca
rosa lee <rosalee0116@gmail.com>
2020-04-23 20:41:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,12 +3,12 @@ class Student < User
   def initialize
     @knowledge = [ ]
   end
-  
+
    def learn(string)
      @knowledge << string
    end
 
-   def knowledge 
+   def knowledge
      @knowledge
    end
 end
